acfee110ea252afe0c098b9c58a3858ce8aff60a
Gustavo Tiago <gu.tiagosilva@gmail.com>
2017-02-17 17:07:55 -0200

    Update erlcloud_aws.erl

--- src/erlcloud_aws.erl
+++ src/erlcloud_aws.erl
@@ -182,8 +182,8 @@ aws_request4_no_update(Method, Protocol, Host, Port, Path, Params, Service,
                         Port :: undefined | integer() | string(), Path :: string(), Form :: string(),
                         Headers :: list(), Config :: aws_config()) -> {ok, binary()} | {error, tuple()}.
 aws_request_form(Method, Protocol, Host, Port, Path, Form, Headers, Config) ->
-    RequestHeaders = [{"content-type",
-                      "application/json; charset=utf-8"} |
+    RequestHeaders = [{"content-type", "application/x-amz-json-1.1"},
+    {"accept", "application/json"} |
                      Headers],
     Scheme = case Protocol of
         undefined -> "https://";
